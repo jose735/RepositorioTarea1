46dd91f918eebbaf8d5ee86be662f778c725d86e
jose735 <jcanoespinoza@gmail.com>
2017-02-10 10:53:01 -0600

    Creacion de la logica del evento

--- Tarea1AsistenciaEventos/AsistenciaEventos.Datos/EventoDatos.cs
+++ Tarea1AsistenciaEventos/AsistenciaEventos.Datos/EventoDatos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,63 @@ namespace AsistenciaEventos.Datos
 {
     public class EventoDatos
     {
+        public void Insertar(String nombre, String fecha, String cierre)
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+            SqlCommand comando = new SqlCommand("PA_Insertar_Evento");
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@Nombre", nombre);
+            comando.Parameters.AddWithValue("@Fecha", fecha);
+            comando.Parameters.AddWithValue("@Cierre", cierre);
+            db.ExecuteNonQuery(comando);
+        }
+
+        public void ModificarFecha(int id, String fecha)
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+
+            SqlCommand comando = new SqlCommand("PA_Modificar_Evento_Fecha");
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@Id", id);
+            comando.Parameters.AddWithValue("@Nombre", fecha);
+            db.ExecuteNonQuery(comando);
+        }
+
+        public void ModificarCierre(int id, String cierre)
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+
+            SqlCommand comando = new SqlCommand("PA_Modificar_Evento_Cierre");
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@Id", id);
+            comando.Parameters.AddWithValue("@Cierre", cierre);
+            db.ExecuteNonQuery(comando);
+        }
+
+        public void Eliminar(int id)
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+
+            SqlCommand comando = new SqlCommand("PA_Eliminar_Evento");
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@Id", id);
+
+            db.ExecuteNonQuery(comando);
+        }
+
+        public DataSet SeleccionarTodos()
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+
+            SqlCommand comando = new SqlCommand("PA_Seleccionar_Todos_Eventos");
+            // Es requerido indicar que el tipo es un StoreProcedure
+            comando.CommandType = CommandType.StoredProcedure;
+
+            DataSet ds = db.ExecuteReader(comando, "Evento");
+            return ds;
+        }
     }
 }
