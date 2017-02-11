eb407278976c7d044ba8d0fa3bbf205f701db5c7
jose735 <jcanoespinoza@gmail.com>
2017-02-11 10:34:29 -0600

    Cambio de inicio de sesion

--- Tarea1AsistenciaEventos/AsistenciaEventos.Grafica/InicioSesion.aspx.cs
+++ Tarea1AsistenciaEventos/AsistenciaEventos.Grafica/InicioSesion.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using AsistenciaEventos.Entidades;
+using AsistenciaEventos.Logica;
 
 namespace AsistenciaEventos.Grafica
 {
     public partial class InicioSesion : System.Web.UI.Page
     {
+        UsuarioLogica usuarioLogica = new UsuarioLogica();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +19,30 @@ namespace AsistenciaEventos.Grafica
 
         protected void btnIngresar_Click(object sender, EventArgs e)
         {
-            Response.Redirect("Principal.aspx");
+            String nombre = txtUsuario.Text;
+            String contrasenna = txtContrasenna.Text;
+
+            if (nombre == "" || contrasenna == "")
+            {
+                cajaError.Visible = true;
+                lblError.CssClass = "text-warning";
+                lblError.Text = "Debe rellenar los campos correspondientes al nombre de usuario y clave de acceso";
+            }
+            else
+            {
+                if (usuarioLogica.SeleccionarTodos(nombre, contrasenna).Count > 0)
+                {
+                    cajaError.Visible = false;
+                    lblError.Text = "";
+                    Response.Redirect("Principal.aspx");
+                }
+                else
+                {
+                    cajaError.Visible = true;
+                    lblError.CssClass = "text-warning";
+                    lblError.Text = "Usuario o contraseña invalidos";
+                }
+            }
         }
     }
 }
